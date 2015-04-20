0e0caaec5b5fd6528356f6ac01775d3a15eb0368
Rob Cameron <rwcameron@gmail.com>
2015-04-20 16:53:38 -0700

    updating ansible path

--- scripts/web-setup.sh
+++ scripts/web-setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export ANSIBLE_LIBRARY=/etc/ansible/roles:/usr/share/pyshared
-if ! grep -qe "export ANSIBLE_LIBRARY=/etc/ansible/roles/" "/home/vagrant/.bashrc"; then
+if ! grep -qe "export ANSIBLE_LIBRARY=/etc/ansible/roles:/usr/share/pyshared" "/home/vagrant/.bashrc"; then
     cat >> /home/vagrant/.bashrc <<EOF
 
 export ANSIBLE_LIBRARY=/etc/ansible/roles:/usr/share/pyshared
